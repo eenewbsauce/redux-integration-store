9e1786bd2979ce28b69a5e5df9f7947075df8253
eenewbsauce <ryan.m.tracey@gmail.com>
2018-02-19 11:47:20 -0500

    feat(webpack): add library to output

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
     entry: './index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: 'redux-integration-store'
     },
     module: {
         rules: [{
