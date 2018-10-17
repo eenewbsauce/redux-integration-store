e257444dfd430b06c8a3c39c6321f80e72f51fa5
eenewbsauce <ryan.m.tracey@gmail.com>
2018-10-17 16:40:32 -0400

    feat(middleware): make mw configurable

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-import thunk from 'redux-thunk';
-import sinon from 'sinon';
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import sinon from "sinon";
 
-function setupIntegrationTest(reducer, reducerKey) {
-    const dispatchSpy = sinon.stub().callsFake(() => ({}));
-    const reducerSpy = (state, action) => dispatchSpy(action);
-    const combinedReducers = combineReducers({
-        reducerSpy,
-        [reducerKey]: reducer,
-    });
+function setupIntegrationTest(reducer, reducerKey, middleware = [thunk]) {
+  const dispatchSpy = sinon.stub().callsFake(() => ({}));
+  const reducerSpy = (state, action) => dispatchSpy(action);
+  const combinedReducers = combineReducers({
+    reducerSpy,
+    [reducerKey]: reducer
+  });
 
-    const store = createStore(combinedReducers, applyMiddleware(thunk));
+  const store = createStore(combinedReducers, applyMiddleware(...middleware));
 
-    return { store, dispatchSpy };
+  return { store, dispatchSpy };
 }
 
 export default setupIntegrationTest;
