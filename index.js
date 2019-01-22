94e085119ba7409f946b2f1e1c1122904dc861b7
eenewbsauce <ryan.m.tracey@gmail.com>
2019-01-22 17:11:05 -0500

    feat(multi-reducer): add support for many reducers simultaneously

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import sinon from "sinon";
 
-function setupIntegrationTest(reducer, reducerKey, middleware = [thunk]) {
+function setupIntegrationTest(
+  reducer,
+  reducerKey,
+  middleware = [thunk],
+  otherReducers = {}
+) {
   const dispatchSpy = sinon.stub().callsFake(() => ({}));
   const reducerSpy = (state, action) => dispatchSpy(action);
   const combinedReducers = combineReducers({
     reducerSpy,
-    [reducerKey]: reducer
+    [reducerKey]: reducer,
+    ...otherReducers
   });
 
   const store = createStore(combinedReducers, applyMiddleware(...middleware));
